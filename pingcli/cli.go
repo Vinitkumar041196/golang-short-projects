0058ebc0d7f7ee145b342d8e8036b9bf4fb628ca
Vinitkumar Gaikwad <vinit.kumar@currencysolutions.com>
2024-09-06 12:38:59 +0100

    Accept and echo input command

--- pingcli/cli.go
+++ pingcli/cli.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -9,4 +11,13 @@ func main() {
 	fmt.Println("Cloud CLI")
 	fmt.Println("------------------")
 	fmt.Print("=>")
+	// Step 2: Accept Command Line Input
+	// Example:
+	// =>exit
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		fmt.Println(userInput)
+		fmt.Print("=>")
+	}
 }
