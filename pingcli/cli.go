8713f579cfd5415dbe5ea3c9ad0bf5796d642204
Vinitkumar Gaikwad <vinit.kumar@currencysolutions.com>
2024-09-06 13:05:19 +0100

    switch implemented for command checking

--- pingcli/cli.go
+++ pingcli/cli.go
@@ -9,34 +9,38 @@ import (
 )
 
 func main() {
-	// Step1: Display Starting Prompt
+	// Display Starting Prompt
 	fmt.Println("Cloud CLI")
 	fmt.Println("------------------")
 	fmt.Print("=>")
-	// Step 2: Accept Command Line Input
+
+	// Accept Command Line Input
 	// Example:
 	// =>exit
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		userInput := scanner.Text()
-
 		var commandString []string = strings.Split(userInput, " ")
 
-		// Step 3: implement exit Command
-		if strings.Compare("exit", commandString[0]) == 0 {
+		switch strings.ToLower(commandString[0]) {
+		case "exit":
+			// implement exit Command
 			os.Exit(0)
-		}
+		case "ping":
+			// implement ping command
+			if argsLen := len(commandString); argsLen < 2 {
+				fmt.Println("Command not supported by the CLI")
+			} else {
+				count := "3"
+				if argsLen > 2 {
+					count = commandString[2]
+				}
 
-		// Step 4: implement ping command
-		if strings.Compare("ping", commandString[0]) == 0 {
-			count := "3"
-			if len(commandString) > 2 {
-				count = commandString[2]
+				commandOutput := execPINGCommand(commandString[1], count)
+
+				fmt.Println(commandOutput)
 			}
-
-			commandOutput := execPINGCommand(commandString[1], count)
-			fmt.Println(commandOutput)
-		} else {
+		default:
 			fmt.Println("Command not supported by the CLI")
 		}
 
@@ -46,7 +50,7 @@ func main() {
 }
 
 func execPINGCommand(cmdParam, count string) string {
-	// Step 5: Execute OS command ping
+	// Execute OS command ping
 	c, err := exec.Command("ping", "-c", count, cmdParam).CombinedOutput()
 	if err != nil {
 		return "Error! Command execution failed: " + err.Error()
